dbb2ca657844881f98f1cbb11d0feef7532d5f02
Hee <Hee.j@pangeadental.com>
2025-07-21 09:55:01 -0600

    responsive v1.6

--- src/data/graphicDesign.ts
+++ src/data/graphicDesign.ts
@@ -3,10 +3,10 @@ const graphicDesign = [
     { id: 2, title: "F&B Menu", image: "/portfolio/bacchus_menu.jpg" },
     { id: 3, title: "Event Planing", image: "/portfolio/hankki_event.jpg" },
     { id: 4, title: "Humorous", image: "/portfolio/hankki_sns.jpg" },
-    { id: 4, title: "Rustic", image: "/portfolio/kims_sns.jpg" },
-    { id: 4, title: "Minimal", image: "/portfolio/ryuko_sns.jpg" },
-    { id: 4, title: "Funky", image: "/portfolio/spacerobo_sns.jpg" },
-    { id: 4, title: "Playful", image: "/portfolio/treehousetoys_sns.jpg" },
+    { id: 5, title: "Rustic", image: "/portfolio/kims_sns.jpg" },
+    { id: 6, title: "Minimal", image: "/portfolio/ryuko_sns.jpg" },
+    { id: 7, title: "Funky", image: "/portfolio/spacerobo_sns.jpg" },
+    { id: 8, title: "Playful", image: "/portfolio/treehousetoys_sns.jpg" },
 ];
 
 export default graphicDesign;
